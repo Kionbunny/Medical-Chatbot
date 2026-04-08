e71db8e62bbb5a6d372c632d7d6322ca65b21efe
ajay <mashapoguajay@gmail.com>
2026-04-08 10:44:11 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+# this file has set-up related code 
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical-chatbot",
+    version = "0.1.0",
+    author = "AJAY_KUMAR",
+    author_email = "mashapoguajay@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
